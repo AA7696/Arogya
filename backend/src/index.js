a502e4eab6f162a07e6b71a1aaf6cdfe0453e678
Aachman Agarwal <achmnaagarwal7696@gmail.com>
2025-04-09 18:47:44 +0530

    backend setup updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,20 @@
-console.log('====================================');
-console.log("Heel");
-console.log('====================================');
+import dotenv from 'dotenv'
+import { app } from './app.js';
+
+
+dotenv.config({
+    path:  "./config/.env"
+})
+
+
+const start = async () =>{
+    try {
+        app.listen(process.env.PORT,() =>{
+            console.log("Server is connected");
+        })
+    } catch (error) {
+        console.log(error);     
+    }
+}
+
+start();
